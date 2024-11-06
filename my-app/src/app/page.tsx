47ea741d08a5296af3ac0ea297b35586342700b7
kw_yeh <kw_yeh@cyberlink.com>
2024-11-06 11:29:25 +0800

    Add Hint

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -6,6 +6,16 @@ import { LOCALES } from '@/utils/constants';
 export default function Home() {
   return (
     <div className="grid w-full min-w-[700px] grid-cols-4 gap-4 p-8">
+      <span className="col-span-4 text-sm">
+        Select a locale and start editing the translation content. Provide a
+        reference JSON link and click load to start editing. After editing,
+        click generate to get a complete JSON file.
+        <br />
+        <span className="text-yellow-500">
+          Note that whenever you switch locales, the editing content will be
+          reset. Please make sure to click generate before switching locales.
+        </span>
+      </span>
       <LocaleProvider>
         <div className="col-span-1 flex flex-col gap-2">
           {LOCALES.map((locale) => (
