2b9b2e5f78fc98ebd4f582d41a46ab191cc41b43
kw_yeh <kw_yeh@cyberlink.com>
2024-11-06 10:43:21 +0800

    Adjust UI

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -5,7 +5,7 @@ import { LOCALES } from '@/utils/constants';
 
 export default function Home() {
   return (
-    <div className="grid w-full grid-cols-4 gap-4 p-8">
+    <div className="grid w-full min-w-[700px] grid-cols-4 gap-4 p-8">
       <LocaleProvider>
         <div className="col-span-1 flex flex-col gap-2">
           {LOCALES.map((locale) => (
