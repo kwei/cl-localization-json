93da1cfd315c0c82dd054041cec6a1b8e61c70f4
kw_yeh <kw_yeh@cyberlink.com>
2024-11-14 15:22:09 +0800

    Make color code to be upper case

--- my-app/src/utils/retrieveFormData.ts
+++ my-app/src/utils/retrieveFormData.ts
@@ -71,56 +71,70 @@ function getThemeData(
   const theme: Theme = { ...originalTheme };
   theme.width =
     (formData.get(`theme-width-${locale}`) as string) ?? originalTheme.width;
-  theme.general.bgColor =
+  theme.general.bgColor = (
     (formData.get(`theme-general-bgColor-${locale}`) as string) ??
-    originalTheme.general.bgColor;
-  theme.general.bgColorHeader =
+    originalTheme.general.bgColor
+  ).toUpperCase();
+  theme.general.bgColorHeader = (
     (formData.get(`theme-general-caption-bgColor-${locale}`) as string) ??
-    originalTheme.general.bgColorHeader;
-  theme.general.primaryFontColor =
+    originalTheme.general.bgColorHeader
+  ).toUpperCase();
+  theme.general.primaryFontColor = (
     (formData.get(`theme-general-primary-font-color-${locale}`) as string) ??
-    originalTheme.general.primaryFontColor;
-  theme.general.secondaryFontColor =
+    originalTheme.general.primaryFontColor
+  ).toUpperCase();
+  theme.general.secondaryFontColor = (
     (formData.get(`theme-general-secondary-font-color-${locale}`) as string) ??
-    originalTheme.general.secondaryFontColor;
-  theme.options.bgColor =
+    originalTheme.general.secondaryFontColor
+  ).toUpperCase();
+  theme.options.bgColor = (
     (formData.get(`theme-option-bgColor-${locale}`) as string) ??
-    originalTheme.options.bgColor;
+    originalTheme.options.bgColor
+  ).toUpperCase();
   theme.options.bgColorHover =
     (formData.get(`theme-option-hover-bgColor-${locale}`) as string) ??
     originalTheme.options.bgColorHover;
   theme.options.bgColorActive =
     (formData.get(`theme-option-active-bgColor-${locale}`) as string) ??
     originalTheme.options.bgColorActive;
-  theme.options.border =
+  theme.options.border = (
     (formData.get(`theme-option-border-color-${locale}`) as string) ??
-    originalTheme.options.border;
-  theme.options.borderHover =
+    originalTheme.options.border
+  ).toUpperCase();
+  theme.options.borderHover = (
     (formData.get(`theme-option-hover-border-color-${locale}`) as string) ??
-    originalTheme.options.borderHover;
-  theme.options.borderActive =
+    originalTheme.options.borderHover
+  ).toUpperCase();
+  theme.options.borderActive = (
     (formData.get(`theme-option-active-border-color-${locale}`) as string) ??
-    originalTheme.options.borderActive;
-  theme.options.primaryFontColor =
+    originalTheme.options.borderActive
+  ).toUpperCase();
+  theme.options.primaryFontColor = (
     (formData.get(`theme-option-primary-font-color-${locale}`) as string) ??
-    originalTheme.options.primaryFontColor;
-  theme.options.primaryFontColorActive =
+    originalTheme.options.primaryFontColor
+  ).toUpperCase();
+  theme.options.primaryFontColorActive = (
     (formData.get(
       `theme-option-active-primary-font-color-${locale}`,
-    ) as string) ?? originalTheme.options.primaryFontColorActive;
-  theme.options.secondaryFontColor =
+    ) as string) ?? originalTheme.options.primaryFontColorActive
+  ).toUpperCase();
+  theme.options.secondaryFontColor = (
     (formData.get(`theme-option-secondary-font-color-${locale}`) as string) ??
-    originalTheme.options.secondaryFontColor;
-  theme.options.secondaryFontColorActive =
+    originalTheme.options.secondaryFontColor
+  ).toUpperCase();
+  theme.options.secondaryFontColorActive = (
     (formData.get(
       `theme-option-active-secondary-font-color-${locale}`,
-    ) as string) ?? originalTheme.options.secondaryFontColorActive;
-  theme.buttons.bgColor =
+    ) as string) ?? originalTheme.options.secondaryFontColorActive
+  ).toUpperCase();
+  theme.buttons.bgColor = (
     (formData.get(`theme-button-bgColor-${locale}`) as string) ??
-    originalTheme.buttons.bgColor;
-  theme.buttons.primaryFontColor =
+    originalTheme.buttons.bgColor
+  ).toUpperCase();
+  theme.buttons.primaryFontColor = (
     (formData.get(`theme-button-primary-font-color-${locale}`) as string) ??
-    originalTheme.buttons.primaryFontColor;
+    originalTheme.buttons.primaryFontColor
+  ).toUpperCase();
 
   return theme;
 }
